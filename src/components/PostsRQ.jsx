cbc00d8eb4b4d822937e1e146ba1f8bdea57825f
Anandhakumar123 <anandhakumarnathan@gmail.com>
2025-11-12 22:48:02 +0530

    mutation implemented

--- src/components/PostsRQ.jsx
+++ src/components/PostsRQ.jsx
@@ -1,19 +1,42 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+
+//GET method
+const fetchposts = () => {
+  return axios.get("http://localhost:4000/posts");
+};
+
+//POST method
+const addPost = (post) => {
+  return axios.post("http://localhost:4000/posts", post);
+};
 
 const PostsRQ = () => {
   // /posts              ["posts"]
   // /posts/1            ["posts", 1]       // 1 is post.id
   // /posts/2/comments   ["posts", 2, "coments"]
 
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
   const { data, isLoading, isError, error, isFetching, refetch } = useQuery({
     queryKey: ["posts"],
-    queryFn: () => {
-      return axios.get("http://localhost:4000/posts");
-    },
-    enabled: false,
+    queryFn: fetchposts,
   });
+
+  const { mutate } = useMutation({
+    mutationFn: addPost,
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newPost = { title, body };
+    mutate(newPost);
+    setTitle("");
+    setBody("");
+  };
 
   if (isLoading) {
     return <div>Page is Loading...</div>;
@@ -29,6 +52,19 @@ const PostsRQ = () => {
 
   return (
     <div className="post-list">
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Enter post title"
+          value={title}
+        />
+        <input
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Enter post body"
+          value={body}
+        />
+        <button type="submit">Post</button>
+      </form>
       <button onClick={refetch}>Fetch posts</button>
       {data?.data.map((post) => (
         <Link to={`/posts-rq/${post.id}`} key={post.id}>
